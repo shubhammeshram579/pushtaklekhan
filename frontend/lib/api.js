7e91cea3794f8989b330d82d21c753e809b5b38b
Shubham Meshram <105120972+shubhammeshram579@users.noreply.github.com>
2026-09-25 07:16:00 +0530

    updated api file

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -150,7 +150,7 @@ import store, { logout } from '../store';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-console.log("API_BASE_URL",API_BASE_URL)
+// console.log("API_BASE_URL",API_BASE_URL)
 
 const api = axios.create({
   baseURL: API_BASE_URL,
